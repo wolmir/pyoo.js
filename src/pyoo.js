ce68ee15beb45880ff681cff16caa8f6a332b0b2
Wolmir Nemitz <wolmir.nemitz@gmail.com>
2016-09-06 13:24:00 -0300

    Add comentarios

--- src/pyoo.js
+++ src/pyoo.js
@@ -29,13 +29,18 @@ function Classe() {
         throw new Error('pyoo.js: Esperado object, obteve Array.');
     }
 
+    // Gambiarra velha. Transforma 'arguments' em um Array.
     var argumentos = Array.prototype.slice.call(arguments);
 
+    // Os primeiros argumentos de Classe são as superclasses.
+    // Portanto, para cada elemento de 'argumentos', exceto o último...
     _(argumentos).initial().each(function(valor) {
+        // Certifica que a superclasse existe...
         if ((!valor) || (valor === null)) {
             throw new Error('pyoo.js: Superclasse inválida, obteve ' + valor);
         }
         
+        // Certifica que a superclasse é do tipo 'Classe'.
         if (!valor.__superclasses) {
             throw new Error('pyoo.js: Esperado Classe, obteve ' + (typeof valor));
         }
@@ -50,22 +55,37 @@ function Classe() {
         }
     });
 
+    // Armazena as propriedades estáticas
     var __estatico = {};
+    // Armazena as superclasses
     var __superclasses = _(argumentos).initial();
 
+    // Copia todas as propriedades estáticas de cada superclasse,
+    // exceto a propriedade '__superclasses' para evitar referências
+    // circulares.
     _(__superclasses).each(function(superclasse) {
         _(superclasse).omit(['__superclasses']).each(function(valor, atributo) {
             __estatico[atributo] = valor;
         });
     });
 
+    // As propriedades do corpo são, por definição, estáticas.
+    // Isso inclui os métodos, o que difere da metodologia
+    // do Python. No entanto, acho uma vantagem não precisar
+    // de decoradores.
     _(corpo).each(function(valor, atributo) {
         __estatico[atributo] = valor;
     });
 
+    // O construtor
     var __classe = (function() {
         var self = {};
 
+        // Copia as propriedades estáticas para 'self'
+        // Se for um método, ele é armazenado na parte estática.
+        // Quando o método for invocado pela instência,
+        // a função estática é invocada passando o 'self'
+        // como primero parâmetro
         _(__estatico).each(function(valor, atributo) {
             if ((typeof valor) === "function") {
                 self[atributo] = function() {
@@ -79,12 +99,19 @@ function Classe() {
             }
         });
 
+        // Invoca o método __init__
         if (__estatico.__init__) {
             __estatico.__init__.apply(self, [self].concat(Array.prototype.slice.call(arguments)));
         }
+
+        // Retorna o 'self' configurado e com
+        // o escopo 'bindado', o que possibilita todas
+        // essas gambiarras.
         return self;
     });
 
+    // Copia o corpo estático para a função construtora
+    // Também seta a propriedade '__superclasses__'
     __classe.__superclasses = __superclasses;
     _(__estatico).each(function(valor, atributo) {
         __classe[atributo] = valor;
